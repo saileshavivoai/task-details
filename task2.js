ca12fbad13af2ae63d5beb219024d75163036621
Abirami <via.abirami15@gmail.com>
2025-06-11 15:37:05 +0530

    Functions

--- task2.js
+++ task2.js
@@ -3,12 +3,8 @@
 // capitalize() to format the name.
 
 function capitalize(name){
-    if(!name){
-        return "";
-    }
-    else{
-        return name.charAt(0).toUpperCase()+name.slice(1);
-    }
+    if(!name) return "";
+    else  return name.charAt(0).toUpperCase()+name.slice(1);
 }
 
 // isEven() to check if age is even
@@ -35,3 +31,37 @@ function personInfo(name, age) {
 let str="abirami";
 let age=20;
 console.log(personInfo(str,age));
+
+
+// Function Types
+// Function Declaration
+
+function myFunction(){
+  console.log("Hello..");
+}
+myFunction();
+
+// Function Expression
+
+let areaOfRect=function(width,height){
+  return width*height;
+}
+console.log(areaOfRect(4,6));
+
+// Arrow Function 
+
+let greet = () => "Hello World!";
+console.log(greet());
+
+// Anonymous Function
+setTimeout(function() {
+  console.log("This runs after 2 seconds");
+}, 2000);
+
+// IIFE (Immediately Invoked Function Expression)
+
+(function() {
+  let message = "A function from IIFE";
+  console.log(message); 
+})();
+
