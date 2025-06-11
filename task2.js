4d041ac117a37c4d255c9a9cd7436510438bf03c
Mona <monachandrasekaran@avivo.ai>
2025-06-11 17:40:25 +0530

    completed

--- task2.js
+++ task2.js
@@ -122,3 +122,120 @@ const found = numb.find(function(num) {
 });
 
 console.log(found); 
+
+
+
+
+//For Loop-The classic loop to run a block a fixed number of times.
+
+for (let i = 0; i < 5; i++) {
+  console.log("For Loop:", i);
+}
+
+//While Loop-Keep running while a condition is true.
+
+let count = 0;
+while (count < 5) {
+  console.log("While Loop:", count);
+  count++;
+}
+
+
+//Do...While Loop-Like while, but runs at least once.
+
+let num1 = 0;
+do {
+  console.log("Do...While Loop:", num1);
+  num1++;
+} while (num1 < 0);
+
+//For...of Loop-Perfect for looping through arrays and strings.
+
+const A = [ 1, 2, 3, 4, 5 ];
+
+for (const item of A) {
+    console.log(item);
+}
+
+
+//For...in Loop-Ideal for iterating over object properties.
+
+const person = {
+  name: "John",
+  age: 30,
+  city: "Delhi"
+};
+
+for (let value in person) {
+  console.log(value);             
+//   console.log(person[key]);     
+} 
+
+
+
+//  Personal Info Formatter
+
+// capitalize() to format the name.
+
+function capitalize(name){
+    if(!name) return "";
+    else  return name.charAt(0).toUpperCase()+name.slice(1);
+}
+
+// isEven() to check if age is even
+
+function isEven(age){
+  return age%2==0;
+}
+
+//  add() to add 5 to the age.
+
+function add(a,b){
+    return a+b;
+}
+
+function personInfo(name, age) {
+  const formattedName = capitalize(name);
+  const evenOrOdd = isEven(age) ? "even" : "odd";
+  const futureAge = add(age, 5);
+
+ return `Hello, ${formattedName}! Your age (${age}) is ${evenOrOdd}. In 5 years, you'll be ${futureAge}.`;
+}
+
+
+let str="monalisa";
+let age=20;
+console.log(personInfo(str,age));
+
+
+// Function Types
+// Function Declaration
+
+function myFunction(){
+  console.log("welcome..");
+}
+myFunction();
+
+// Function Expression
+
+let areaOfRect=function(width,height){
+  return width*height;
+}
+console.log(areaOfRect(4,6));
+
+// Arrow Function 
+
+let greet = () => "Hello World!";
+console.log(greet());
+
+// Anonymous Function
+setTimeout(function() {
+  console.log("This runs after 4 seconds");
+}, 4000);
+
+// IIFE (Immediately Invoked Function Expression)
+
+(function() {
+  let message = "A function from IIFE";
+  console.log(message); 
+})();
